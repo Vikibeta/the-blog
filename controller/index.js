c241ffb77f81605c807658854e7b21ea090b8f32
King <wyk17@qq.com>
2017-06-07 20:16:37 +0800

    format

--- controller/index.js
+++ controller/index.js
@@ -1,27 +1,29 @@
 var Post = require("../models/post");
 
-module.exports = function(req,res,next){
-  var name = '';
-  if(req.session.user){
-    if(req.session.user.ops){
-      name = req.session.user.ops[0].name;
-    }else{
-      name = req.session.user.name;
+module.exports = function(req, res, next) {
+    var name = '';
+    if (req.session.user) {
+        if (req.session.user.ops) {
+            name = req.session.user.ops[0].name;
+        } else {
+            name = req.session.user.name;
+        }
+    } else {
+        name = null;
     }
-  }else{
-    name = null;
-  }
 
-  Post.getAll(name, function (err, posts) {
-    if (err) {
-      posts = [];
-    }
-    res.render('index', {
-      title: '主页',
-      user: req.session.user,
-      posts: posts,
-      success: req.flash('success').toString(),
-      error: req.flash('error').toString()
+    Post.getAll(name, function(err, posts) {
+        console.log(name)
+        if (err) {
+            console.log(err)
+            posts = [];
+        }
+        res.render('index', {
+            title: '主页',
+            user: req.session.user,
+            posts: posts,
+            success: req.flash('success').toString(),
+            error: req.flash('error').toString()
+        });
     });
-  });
 };
